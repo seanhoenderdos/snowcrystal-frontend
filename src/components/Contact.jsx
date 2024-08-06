0bef4bae9097a72595654ee8bf1fadc5108fbfbe
seanhoenderdos <101661395+seanhoenderdos@users.noreply.github.com>
2024-08-06 15:54:54 +0200

    updated html and contact to ensure form works

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,33 +17,53 @@ const Contact = () => {
             className="space-y-6 bg-n-8 rounded-lg shadow-md max-w-xl w-full"
           >
             <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
             <div>
-              <label className="block text-sm font-medium text-n-1">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-n-1"
+              >
                 Your Name
               </label>
               <input
                 type="text"
                 name="name"
+                id="name"
                 className="mt-1 block w-full px-3 py-2 border border-stroke-1 rounded-md shadow-sm focus:outline-none focus:ring-[#FFB6C1] focus:border-[#FFB6C1] sm:text-sm text-n-4"
+                autoComplete="name"
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-n-1">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-n-1"
+              >
                 Your Email
               </label>
               <input
                 type="email"
                 name="email"
+                id="email"
                 className="mt-1 block w-full px-3 py-2 border border-stroke-1 rounded-md shadow-sm focus:outline-none focus:ring-[#FFB6C1] focus:border-[#FFB6C1] sm:text-sm text-n-4"
+                autoComplete="email"
               />
             </div>
             <div>
-              <label className="block text-sm font-medium text-n-1">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-n-1"
+              >
                 Your Message
               </label>
               <textarea
                 name="message"
+                id="message"
                 className="mt-1 block w-full px-3 py-2 border border-stroke-1 rounded-md shadow-sm focus:outline-none focus:ring-[#FFB6C1] focus:border-[#FFB6C1] sm:text-sm text-n-4"
+                autoComplete="off"
               ></textarea>
             </div>
             <div className="flex lg:justify-center">
